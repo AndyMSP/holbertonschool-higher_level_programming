b5a529e9227a34004a4d7438b5003f45d6d08c89
Andy <3775@holbertonschool.com>
2022-05-01 22:49:05 +0000

    Debug task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for a in range(10):
     for b in range(10):
-        if b > a and (a != 8 and b != 9):
+        if b > a and not (a == 8 and b == 9):
             print(f"{a}{b}, ", end='')
 print(f"89")
