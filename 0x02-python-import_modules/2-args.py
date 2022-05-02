416f8d275f096f22e5f9141035803a9aae81a78e
Andy <3775@holbertonschool.com>
2022-05-02 22:07:56 +0000

    Add task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import sys
+num = len(sys.argv) - 1
+print(f"{num} argument{'s.' if num == 0 else(':' if num == 1 else 's:')}")
+for i in range(1, num + 1):
+    print(f"{i}: {sys.argv[i]}")
