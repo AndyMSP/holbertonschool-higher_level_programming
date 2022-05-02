27928ada895b6b62e1840ceb0c355f37b22a891d
Andy <3775@holbertonschool.com>
2022-05-02 22:12:17 +0000

    Name guard task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import sys
-num = len(sys.argv) - 1
-print(f"{num} argument{'s.' if num == 0 else(':' if num == 1 else 's:')}")
-for i in range(1, num + 1):
-    print(f"{i}: {sys.argv[i]}")
+if __name__ == "__main__":
+    import sys
+    num = len(sys.argv) - 1
+    print(f"{num} argument{'s.' if num == 0 else(':' if num == 1 else 's:')}")
+    for i in range(1, num + 1):
+        print(f"{i}: {sys.argv[i]}")
