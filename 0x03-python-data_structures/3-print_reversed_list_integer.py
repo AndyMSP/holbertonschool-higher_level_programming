76a3f5711f273d681260353946d95532baada32b
Andy <3775@holbertonschool.com>
2022-05-03 05:00:54 +0000

    Add edge cases to task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list):
+def print_reversed_list_integer(my_list=[]):
+    if my_list == None:
+        return None
+    for i in my_list:
+        if type(i) is not int:
+            return None
     my_list.reverse()
     for i in my_list:
         print(i)
