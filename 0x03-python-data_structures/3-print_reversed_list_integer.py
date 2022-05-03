88c39e4e3976692f2096fe68c8704014030ca942
Andy <3775@holbertonschool.com>
2022-05-03 05:04:31 +0000

    Task 3 edge case

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,9 +2,6 @@
 def print_reversed_list_integer(my_list=[]):
     if my_list is None:
         return None
-    for i in my_list:
-        if type(i) is not int:
-            return None
     my_list.reverse()
     for i in my_list:
         print(i)
