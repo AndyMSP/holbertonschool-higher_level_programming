171a716a73e323c29086d80396c40d0b7419abac
Andy <3775@holbertonschool.com>
2022-05-25 16:09:39 +0000

    Debug

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,7 @@
-"""This module defines an empty class called square"""
 #!/usr/bin/python3
+"""This module defines an empty class called square"""
+
+
 class Square:
     """This is an empty class to demonstrate creation of a class"""
     pass
