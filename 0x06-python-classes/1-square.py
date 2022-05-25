cf48d38fe7aded6fde18148b6b094704268d9b30
Andy <3775@holbertonschool.com>
2022-05-25 17:32:05 +0000

    Debug

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """This module defines a class called square"""
+
+
 class Square:
+    """This class represents a square"""
     def __init__(self, size):
         self.__size = size
