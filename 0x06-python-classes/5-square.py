b064205e1df3bea74bb379391f90659fbeeaf241
Andy <3775@holbertonschool.com>
2022-05-26 03:58:27 +0000

    Add task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""This module defines a class called square"""
+
+
+class Square:
+    """This class represents a square"""
+    def __init__(self, size=0):
+        self.size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        for i in range(self.size):
+            for j in range(self.size):
+                print("#", end="")
+            print()
+        if self.size == 0:
+            print()
