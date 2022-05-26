58bce9caab054290c02191f80299edb870d1acf6
Andy <3775@holbertonschool.com>
2022-05-26 04:43:51 +0000

    Add task 6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""This module defines a class called square"""
+
+
+class Square:
+    """This class represents a square"""
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    def my_print(self):
+        for i in range(self.__size):
+            print(" " * self.__position[0], end="")
+            print("#" * self.__size)
+        print()
