dc31563e8aeb8b336e98ef39262e0288841e2fa1
Andy <3775@holbertonschool.com>
2022-05-26 02:15:36 +0000

    Debug

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """This class represents a square"""
-    def __init__(self, size=0):
+    ef __init__(self, size=0):
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
