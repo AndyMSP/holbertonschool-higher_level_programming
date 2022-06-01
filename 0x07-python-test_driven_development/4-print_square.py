d4e45f400f36a3d0edd656acfe05c9e2cb7b7a4d
Andy <3775@holbertonschool.com>
2022-06-01 16:40:36 +0000

    Debug

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -3,9 +3,10 @@
 
 
 def print_square(size):
+    """Function to print a square with hashtags"""
     if type(size) is not int:
         raise TypeError("size must be an integer")
-    elif size <= 0:
+    elif size < 0:
         raise ValueError("size must be >= 0")
     else:
         for i in range(size):
