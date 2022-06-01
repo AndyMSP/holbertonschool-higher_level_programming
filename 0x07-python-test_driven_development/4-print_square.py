c6f834fff9f83ad0a9878ae96cf42f0fcc2f19ec
Andy <3775@holbertonschool.com>
2022-06-01 16:38:05 +0000

    Debug

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -5,7 +5,7 @@
 def print_square(size):
     if type(size) is not int:
         raise TypeError("size must be an integer")
-    elif size == 0:
+    elif size <= 0:
         raise ValueError("size must be >= 0")
     else:
         for i in range(size):
