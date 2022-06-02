dffc19771b92565e61d2af192b2ffa63a620b437
Andy <3775@holbertonschool.com>
2022-06-02 00:24:28 +0000

    Add task 3

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""This module defines an empty class called Rectangle"""
+
+
+class Rectangle:
+    """This Rectangle has a positive integer height and width"""
+
+    def __init__(self, width=0, height=0):
+        """Method executed when new Rectangle instance is created"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Method to get private variable __width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Method to set private variable __width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Method to get private variable __height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Method to set private variable __height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Method to calculate and return area of rectangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Method to calculate and return perimeter of rectangle"""
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * self.__width + 2 * self.__height
+
+    def __str__(self):
+        """Method to define __str__"""
+        return (("#" * self.width + "\n") * self.height)[:-1]
