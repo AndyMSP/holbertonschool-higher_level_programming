3d7733c1f668266a1236dcc663100e6c6030d980
Andy <3775@holbertonschool.com>
2022-06-07 20:05:54 +0000

    Add task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module containing the definition for a class of type square"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class representing a square"""
+
+    def __init__(self, size):
+        """method to be called on instantiation"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """method to return 'string value' of object"""
+        return f"[Square] {self.__size}/{self.__size}"
