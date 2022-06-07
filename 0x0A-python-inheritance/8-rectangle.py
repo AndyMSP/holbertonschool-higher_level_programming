921661912f30ef7d6902844d05da1a10fc404499
Andy <3775@holbertonschool.com>
2022-06-07 19:13:29 +0000

    Add task 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module defines a class of type Rectangle"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Class of type Rectangle"""
+
+    def __init__(self, width, height):
+        """function called on instantiation"""
+        super().integer_validator(self, width)
+        super().integer_validator(self, height)
+        __width = width
+        __height = height
