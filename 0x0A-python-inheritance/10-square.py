b4065a5cc1137e88f99441f769d307a93afb8ecf
Andy <3775@holbertonschool.com>
2022-06-07 19:59:06 +0000

    Debug

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -9,5 +9,6 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """method to be called on instantiation"""
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
