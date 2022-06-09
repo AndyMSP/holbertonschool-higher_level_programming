3ecae3cb2fee2ebd044df1145b518c1f1ee7058a
Andy <3775@holbertonschool.com>
2022-06-09 15:07:27 +0000

    Add task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module contains function to open read and close a file"""
+
+
+def read_file(filename=""):
+    """function to open a file, read it, print it and close the file"""
+    with open(filename, 'r', encoding='utf-8') as f:
+        print(f.read())
