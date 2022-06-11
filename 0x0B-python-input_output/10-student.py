0f2cf4e629bc152919458d04ec04e4b8c64e3d23
Andy <3775@holbertonschool.com>
2022-06-11 17:18:05 +0000

    Add task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module containing class definition for Student"""
+
+
+class Student:
+    """Definition for Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Method called on instantiation of new Student instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Method to return dictionary representation of Student"""
+        d = self.__dict__
+        if attrs is None:
+            return d
+        else:
+            tmp = set(d) & set(attrs)
+            new_d = dict()
+            for k in tmp:
+                new_d.update({k: d.get(k)})
+            return new_d
