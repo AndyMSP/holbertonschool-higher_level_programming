3b3be0ecca97e968f49ce04d0efad0ab95a5bd81
Andy <3775@holbertonschool.com>
2022-06-19 14:24:26 +0000

    Debug

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -7,9 +7,10 @@ def text_indentation(text):
     printed after each '.', ':' and '?'.  White spaces at the beginning and
     end of each new line should be removed"""
     try:
-        text = text.replace('?', '.')
-        text = text.replace(':', '.')
-        li = text.rsplit('.')
+        text = text.replace('.', '.split')
+        text = text.replace('?', '?split')
+        text = text.replace(':', ':split')
+        li = text.rsplit('split')
         li = [i.strip() for i in li]
         [print(i+'\n') for i in li[:-1]]
         print(li[-1], end='')
