259378cd03f82e6d645b4e09c6b8469e02fa5868
Andy <3775@holbertonschool.com>
2022-06-19 20:15:28 +0000

    Debug

---
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Unit tests for base"""
+
+
+import sys
+sys.path.append('/home/vagrant/projects/holbertonschool-\
+higher_level_programming/0x0C-python-almost_a_circle')
+import unittest
+from models.base import Base
