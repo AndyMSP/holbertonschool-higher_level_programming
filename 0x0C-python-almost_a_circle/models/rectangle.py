1dd16d27d9bad3e9973869d4c0fb60017b3cb744
Andy <3775@holbertonschool.com>
2022-06-20 01:29:50 +0000

    Debug

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""Module defining Rectangle class"""
+
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Rectangle class definition"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Method to be called on instantiation of new Rectangle object"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Method to retrieve value of private variable __width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Method to set value of private variable __width"""
+        self.__width = value
+
+    @property
+    def height(self):
+        """Method to retrieve value of private variable __height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Method to set value of private variable __height"""
+        self.__height = value
+
+    @property
+    def x(self):
+        """Method to retrieve value of private variable __x"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Method to set value of private variable __x"""
+        self.__x = value
+
+    @property
+    def y(self):
+        """Method to retrieve value of private variable __y"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Method to set value of private variable __y"""
+        self.__y = value
---
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Unit tests for Rectangle"""
+
+
+import unittest
+from models.rectangle import Rectangle
+
+
+class Test_2(unittest.TestCase):
+    """Unit tests for task 2"""
+    def test_rect_instantiation(self):
+        """Test that object can be instantiated given various inputs"""
+        r1 = Rectangle(2, 3, 4, 5, 6)
+        r2 = Rectangle(7, 8)
+        self.assertAlmostEqual(r1.id, 6)
+        self.assertAlmostEqual(r1.width, 2)
+        self.assertAlmostEqual(r1.height, 3)
+        self.assertAlmostEqual(r1.x, 4)
+        self.assertAlmostEqual(r1.y, 5)
+        self.assertAlmostEqual(r2.id, 1)
+        self.assertAlmostEqual(r2.width, 7)
+        self.assertAlmostEqual(r2.height, 8)
+        self.assertAlmostEqual(r2.x, 0)
+        self.assertAlmostEqual(r2.y, 0)
