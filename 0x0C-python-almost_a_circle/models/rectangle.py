94e5c5eb43ab2eae0f6890bc86aea5a298124939
Andy <3775@holbertonschool.com>
2022-06-28 22:42:49 +0000

    Debug

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -115,3 +115,8 @@ class Rectangle(Base):
             self.x = kwargs['x']
         if 'y' in kwargs:
             self.y = kwargs['y']
+
+    def to_dictionary(self):
+        """Return dictionary representation of rectangle"""
+        return {'id': self.id, 'width': self.width, 'height': self.height,
+                'x': self.x, 'y': self.y}
