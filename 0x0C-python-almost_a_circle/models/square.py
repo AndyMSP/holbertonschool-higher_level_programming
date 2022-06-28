b2ba156d1462edb472ab0461404f9f4098d36a6e
Andy <3775@holbertonschool.com>
2022-06-28 22:09:34 +0000

    Debug

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,11 +10,18 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         """Called on instantiation of new Square object"""
+        self.size = size;
         super().__init__(size, size, x, y, id)
 
-    @property
-    def size(self):
-        """Method to retrieve value of private"""
+    def size_setter(self):
+        """Method to set value of variable size"""
+        self.width = self.size
+        self.height = self.size
+
+    def size_getter(self):
+        """Method to retrieve size variable"""
+        return self.size
+        
 
     def __str__(self):
         """return string representation of object"""
