4cea8f933a7580b6b3b13517cdf78159c1737e35
Andy <3775@holbertonschool.com>
2022-06-28 22:31:10 +0000

    Add task 12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -29,3 +29,24 @@ class Square(Rectangle):
             f"[Square] ({self.id}) {self.x}/{self.y} - "
             f"{self.width}"
         )
+
+    def update(self, *args, **kwargs):
+        """Update object with supplied arguments"""
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.size = args[1]
+        if len(args) > 2:
+            self.x = args[2]
+        if len(args) > 3:
+            self.y = args[3]
+        if len(args) > 0:
+            return
+        if 'id' in kwargs:
+            self.id = kwargs['id']
+        if 'size' in kwargs:
+            self.size = kwargs['size']
+        if 'x' in kwargs:
+            self.x = kwargs['x']
+        if 'y' in kwargs:
+            self.y = kwargs['y']
