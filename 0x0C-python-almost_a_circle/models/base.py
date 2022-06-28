4350fba7d230cb5c79181be3238048e3a834ec62
Andy <3775@holbertonschool.com>
2022-06-28 23:09:23 +0000

    Debug

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -27,5 +27,5 @@ class Base:
     def to_json_string(list_dictionaries):
         """Return JSON string representation of a list of dictionaries"""
         if list_dictionaries is None or list_dictionaries == []:
-            return []
+            return '[]'
         return json.dumps(list_dictionaries)
