dd0a481184dea1d7f389e0c74cce6073ebb8d6cb
Andy <3775@holbertonschool.com>
2022-06-29 22:15:37 +0000

    Debug

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -59,6 +59,6 @@ class Base:
     @classmethod
     def create(cls, **dictionary):
         """create objects from dictionary"""
-        obj = cls(1, 1)
+        obj = cls(**dictionary)
         obj.update(**dictionary)
         return obj
