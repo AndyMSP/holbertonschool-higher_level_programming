b7c9e69a6e68760e7b061298b8683373f0a37f82
Andy <3775@holbertonschool.com>
2022-08-03 21:19:38 -0400

    debug

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 
 const process = require('process');
-// const av = process.argv;
-const x = Number(Math.floor(process.argv[2]));
-// const x = parseInt(av[2]);
+const av = process.argv;
+const x = parseInt(av[2]);
 if (isNaN(x)) {
   console.log('Missing size');
 } else {
