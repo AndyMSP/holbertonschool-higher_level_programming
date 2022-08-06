816b7e8255e0388e9831e4abf09034c42b63af63
Andy <3775@holbertonschool.com>
2022-08-06 11:15:17 -0400

    Fix semistandard

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,10 +4,11 @@ const process = require('process');
 const av = process.argv;
 
 function fact (x) {
-    if (!(x >= 1))
-        return (1);
-    else
-        return ( x * fact(x - 1));
+  if (!(x >= 1)) {
+    return (1);
+  } else {
+    return (x * fact(x - 1));
+  }
 }
 
-console.log (fact(av[2]));
+console.log(fact(av[2]));
