83dc7d03ca8e696d50c6355a3b2977a53fbbbecf
Andy <3775@holbertonschool.com>
2022-08-08 14:10:44 -0400

    Add task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Lists all states in a database"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    MY_HOST = 'localhost'
+    MY_USER = sys.argv[1]
+    MY_PASS = sys.argv[2]
+    MY_DB = sys.argv[3]
+
+    # Connect to database
+    db = MySQLdb.connect(host=MY_HOST, user=MY_USER, passwd=MY_PASS, db=MY_DB)
+
+    # Create cursor
+    cur = db.cursor()
+
+    # Perform queries
+    cur.execute('SELECT * FROM states ORDER BY id ASC')
+    states = cur.fetchall()
+    for record in states:
+        print(record)
