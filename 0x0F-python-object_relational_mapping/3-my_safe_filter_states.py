19de0ef9216fa9fb214670a3f06fde57185ac553
Andy <3775@holbertonschool.com>
2022-08-08 17:25:45 -0400

    Add task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Lists all values from table matching user input"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    MY_HOST = 'localhost'
+    MY_USER = sys.argv[1]
+    MY_PASS = sys.argv[2]
+    MY_DB = sys.argv[3]
+    match = sys.argv[4]
+
+    # Connect to database
+    db = MySQLdb.connect(host=MY_HOST, user=MY_USER, passwd=MY_PASS, db=MY_DB)
+
+    # Create cursor
+    cur = db.cursor()
+
+    # print(query)
+    cur.execute("SELECT * FROM states WHERE \
+        name = %s ORDER BY states.id ASC", (match,))
+    states = cur.fetchall()
+    for record in states:
+        if record[1] == match:
+            print(record)
+
+    # Close cursors and databases
+    cur.close()
+    db.close()
