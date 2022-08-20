505afb2e100c86f281987f721e2bcaabf7a95d28
Andy <3775@holbertonschool.com>
2022-08-19 21:43:29 -0400

    sfasfdds

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""Fetches a holberton intranet link"""
+"""fetches a link and formats body contents"""
+
 import urllib.request
 
-
 if __name__ == "__main__":
-    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
-        body = response.read()
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+        print(f"\t- type: {type(content)}")
+        print(f"\t- content: {content}")
+        print(f"\t- utf8 content: {content.decode('utf8')}")
