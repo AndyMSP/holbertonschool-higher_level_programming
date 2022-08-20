c421865abbd74cb0989b48ce2e40ce314c8f2c23
Andy <3775@holbertonschool.com>
2022-08-19 21:28:16 -0400

    import protection

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 # fetches https://intranet.hbtn.io/status and formats body contents
 
-import urllib.request
+if __name__ == "__main__":
+    import urllib.request
 
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    content = response.read()
-    print("Body response:")
-    print(f"\t- type: {type(content)}")
-    print(f"\t- content: {content}")
-    print(f"\t- utf8 content: {content.decode('utf8')}")
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
+        print("Body response:")
+        print(f"\t- type: {type(content)}")
+        print(f"\t- content: {content}")
+        print(f"\t- utf8 content: {content.decode('utf8')}")
