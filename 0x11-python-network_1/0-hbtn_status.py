83671250ebb50c91b4f612558057351ed94fc493
Andy <3775@holbertonschool.com>
2022-08-19 21:37:02 -0400

    move import

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """fetches a link and formats body contents"""
 
+import urllib.request
+
 if __name__ == "__main__":
-    import urllib.request
 
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         content = response.read()
