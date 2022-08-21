8f810297cd2ecab7e5169469f1382a5a8a30f7fd
Andy <3775@holbertonschool.com>
2022-08-20 21:46:51 -0400

    brake it

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-""" Module fetches url """
+"""Use request package to fetch web resource"""
 
 import requests
 
 if __name__ == '__main__':
-    response = requests.get('https://intranet.hbtn.io/status')
+
+    r = requests.get('http://0.0.0.0:5050/status')
     print('Body response:')
-    print('\t- type: {}'.format(type(response.text)))
-    print('\t- content: {}'.format(response.text))
+    print('\t- type: {}'.format(type(r.text)))
+    print('\t- content: {}'.format(r.text))
