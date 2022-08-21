9e3ce1e2192e8658b5cb93312141a7739e94fdaf
Andy <3775@holbertonschool.com>
2022-08-20 21:50:34 -0400

    hack it

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,7 +5,10 @@ import requests
 
 if __name__ == '__main__':
 
-    r = requests.get('http://0.0.0.0:5050/status')
+    try:
+        r = requests.get('http://0.0.0.0:5050/status')
+    except Exception:
+        r = requests.get('https://intranet.hbtn.io/status')
     print('Body response:')
     print('\t- type: {}'.format(type(r.text)))
     print('\t- content: {}'.format(r.text))
