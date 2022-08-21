26a3f577696157b99ddae5e3976e5a2df00aef18
Andy <3775@holbertonschool.com>
2022-08-20 21:41:02 -0400

    Remove double quotes

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,9 +3,9 @@
 
 import requests
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
     r = requests.get('http://intranet.hbtn.io/status')
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    print('Body response:')
+    print('\t- type: {}'.format(type(r.text)))
+    print('\t- content: {}'.format(r.text))
