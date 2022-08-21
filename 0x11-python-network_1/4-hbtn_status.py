795711912721d17173d327d15bbabf8cb51d5cf6
Andy <3775@holbertonschool.com>
2022-08-20 21:34:45 -0400

    reformat print statement

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
 
     r = requests.get('http://intranet.hbtn.io/status')
     print("Body response:")
-    print(f"\t- type: {type(r.text)}")
-    print(f"\t- content: {r.text}")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
