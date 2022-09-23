0971c4b897e002a7d909a46f1826e212ffeda7ac
Andy <3775@holbertonschool.com>
2022-09-23 17:04:43 -0400

    Add task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,3 +1,27 @@
 #!/usr/bin/node
 
-console.log(3)
+const process = require('process');
+const axios = require('axios');
+
+const url = process.argv[2];
+
+async function numMovies (url, id) {
+  try {
+    let count = 0;
+    const response = await axios(url);
+    const data = response.data;
+    const numFilms = data.count;
+    const films = data.results;
+    for (let i = 0; i < numFilms; i++) {
+      if (films[i].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
+        count++;
+      }
+    }
+    console.log(count);
+  } catch (error) {
+    console.log('error');
+    console.log(error.response.status);
+  }
+}
+
+numMovies(url, 18);
