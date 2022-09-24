71d9d10a2dc8e2199f070f2e07901fb01f3eb329
Andy <3775@holbertonschool.com>
2022-09-24 01:18:30 -0400

    Fix again

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -11,7 +11,7 @@ axios.get(url)
   .then(response => {
     const todos = response.data;
     todos.forEach(task => {
-      if (count[task.userId] === undefined) {
+      if (count[task.userId] === undefined && task.completed) {
         count[task.userId] = 0;
       }
       if (task.completed) {
