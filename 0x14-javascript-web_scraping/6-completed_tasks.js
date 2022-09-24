57aea03466587c327625384fcf7880dfdf502d5d
Andy <3775@holbertonschool.com>
2022-09-24 00:55:29 -0400

    Add task 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const process = require('process');
+const axios = require('axios');
+
+const url = process.argv[2] + '?completed=true';
+
+const count = [];
+
+axios.get(url)
+  .then(response => {
+    const todos = response.data;
+    todos.forEach(task => {
+      if (count[task.userId] === undefined) {
+        count[task.userId] = 0;
+      }
+      count[task.userId]++;
+    });
+    for (const i in count) {
+      if (count[i] !== 0) {
+        console.log('\'' + i + '\': ' + count[i]);
+      }
+    }
+  });
