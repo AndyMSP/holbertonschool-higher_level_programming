b1a5ce9fcdd176a03d9c4c5b428b9f8539bab3d7
andy <astonemn@gmail.com>
2025-12-05 18:35:33 -0600

    Initial commit

---
+++ 0x02-python-import_modules/1-calculator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+
+    sum = add(a, b)
+    diff = sub(a, b)
+    prod = mul(a, b)
+    quo = div(a, b)
+
+    print(f"{a} + {b} = {sum}")
+    print(f"{a} - {b} = {diff}")
+    print(f"{a} * {b} = {prod}")
+    print(f"{a} / {b} = {quo}")
