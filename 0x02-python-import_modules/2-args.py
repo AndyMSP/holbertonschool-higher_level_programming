90e7ea7c36d686d45b423753e73093a454dd2acc
andy <astonemn@gmail.com>
2025-12-05 19:03:46 -0600

    Initial commit

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    args = sys.argv
+    if len(args) == 1:
+        print("0 arguments")
+    elif len(args) == 2:
+        print("1 argument:")
+    else:
+        print(f"{len(args) - 1} arguments:")
+
+    for i in range(1, len(args)):
+        print(f"{i}: {args[i]}")
