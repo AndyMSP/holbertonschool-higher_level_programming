6d42f5420569335a9a76d5a4fc1108229e652ba7
andy <astonemn@gmail.com>
2025-12-14 19:10:21 -0600

    Initial commit

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def search_replace(my_list, search, replace):
+    new_list = my_list.copy()
+    i = 0
+    for item in new_list:
+        if item == search:
+            new_list[i] = replace
+        i += 1
+    return new_list
