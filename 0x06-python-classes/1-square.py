250aebd88e47673035e2d7662f1d7e6ad8ef8533
andy <astonemn@gmail.com>
2025-12-19 13:37:36 -0600

    Initial commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module provides a class that defines a square"""
+
+
+class Square:
+    """This class defines a square object"""
+
+    def __init__(self, size):
+        self.__size = size
