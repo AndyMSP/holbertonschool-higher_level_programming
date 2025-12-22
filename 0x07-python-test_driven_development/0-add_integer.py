dfb69c08a3f476b04c80eed67f3f17aa5fd062cd
andy <astonemn@gmail.com>
2025-12-21 22:14:26 -0600

    Retry documentation

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""Line 1 to fix documentation
-Module containing the add_integer function
-Add_integer raises errors appropriately
-Used to practice doctest feature
+"""
+Module containing various functions
+
+add_integer for adding two numbers together
 """
 
 
 def add_integer(a, b=98):
-    """Line 1 to fix documentaion
+    """
     Adds two numbers and returns value
-    line 3 to fix documentation"""
+    """
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
