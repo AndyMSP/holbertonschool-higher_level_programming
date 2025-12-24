3aa693b5995d9f0194edd8b74cf6f534bf83df27
andy <astonemn@gmail.com>
2025-12-23 22:14:10 -0600

    initial commit

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Module containing Rectangle class and test script"""
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Representation of a rectangle"""
+
+    def __init__(self, width, height):
+        """Iniitalize rectangle"""
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """return calculated area of rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return human readable string representation of Rectangle instance"""
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+
+if __name__ == "__main__":
+    r = Rectangle(3, 5)
+
+    print(r)
+    print(r.area())
