42ef248e4d7c002ab1e21906de230a1dce45aef3
andy <astonemn@gmail.com>
2025-12-23 19:34:35 -0600

    initial commit

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""A module containing a class derived from list"""
+
+
+class MyList(list):
+    """sub-class of list"""
+
+    def print_sorted(self):
+        """prints a list of ints sorted ascending"""
+        s = self[:]
+        s.sort()
+        print(s)
+
+
+if __name__ == "__main__":
+    l1 = MyList([9, 0, 2, 6, 4, 5])
+    print(l1)
+    l1.print_sorted()
+    print(l1)
