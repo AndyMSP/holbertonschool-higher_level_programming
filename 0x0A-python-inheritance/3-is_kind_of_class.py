0f8beed7d4627478201ee21e4f8b62acae8f08c3
andy <astonemn@gmail.com>
2025-12-23 19:48:01 -0600

    initial commit

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module containing function and test script"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Function to determine if an obect is an instance of a class"""
+    return isinstance(obj, a_class)
