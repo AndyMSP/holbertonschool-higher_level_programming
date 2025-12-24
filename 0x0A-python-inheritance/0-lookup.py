8a05983895027f90a1595657480353c427f85211
andy <astonemn@gmail.com>
2025-12-23 18:41:41 -0600

    Revert back to my code - accept checker failure

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
-"""
-This module contains a function call
-lookup that returns a list of availables
-attributes and method of the pass object
-"""
+"""This module contains a function lookup"""
 
 
 def lookup(obj):
-    """
-    This function returns a list of availables
-    attributes and methos of an object
-    passed to the function.
-    """
+    """return a list of attributes and methods of an object"""
     return dir(obj)
